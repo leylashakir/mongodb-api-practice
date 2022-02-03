83790411729a8311b1d939e8ea8a3f51c8c39560
Glitch (twisty-kindhearted-napkin) <none>
2022-02-03 23:24:56 +0000

    🐇👏 Checkpoint ./myApp.js:19135473/336

--- myApp.js
+++ myApp.js
@@ -2,8 +2,21 @@ require('dotenv').config();
 var mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+const { Schema } = mongoose;
+const personSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  }, 
+  age: {
+    type: Number,
+  },
+  favoriteFoods: [{
+    type: String
+  }]
+})
 
-let Person;
+let Person = mongoose.model('Person', personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
